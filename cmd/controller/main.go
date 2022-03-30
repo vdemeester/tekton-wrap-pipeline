2ce3b9589181d60c88575bc196e38f4140e92e1c
Vincent Demeester <vdemeest@redhat.com>
2022-03-30 14:07:20 +0200

    Fix ControllerLogKey…
    … to not use an existing one.
    
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	// ControllerLogKey is the name of the logger for the controller cmd
-	ControllerLogKey = "tekton-pipelines-controller"
+	ControllerLogKey = "tekton-oci-pipelines-controller"
 )
 
 func main() {
