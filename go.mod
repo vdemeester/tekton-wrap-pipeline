2f5dff1f4b7c9350716a178ac8b37649ecb00ebb
Vincent Demeester <vdemeest@redhat.com>
2022-09-13 10:14:22 +0200

    Initial slightly working wrap in oci resolver
    Signed-off-by: Vincent Demeester <vdemeest@redhat.com>

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/openshift-pipelines/tekton-wrap-pipeline
 go 1.18
 
 require (
-	github.com/google/go-containerregistry/pkg/authn/k8schain v0.0.0-20220328141311-efc62d802606
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/tektoncd/pipeline v0.39.1-0.20220910000830-4abedf046ddd
 	go.uber.org/zap v1.23.0
@@ -12,50 +11,21 @@ require (
 	k8s.io/apimachinery v0.23.10
 	k8s.io/client-go v0.23.10
 	knative.dev/pkg v0.0.0-20220818004048-4a03844c0b15
+	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
-	cloud.google.com/go/compute v1.5.0 // indirect
 	contrib.go.opencensus.io/exporter/ocagent v0.7.1-0.20200907061046-05415f1de66d // indirect
 	contrib.go.opencensus.io/exporter/prometheus v0.4.0 // indirect
-	github.com/Azure/azure-sdk-for-go v63.3.0+incompatible // indirect
-	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
-	github.com/Azure/go-autorest/autorest v0.11.24 // indirect
-	github.com/Azure/go-autorest/autorest/adal v0.9.18 // indirect
-	github.com/Azure/go-autorest/autorest/azure/auth v0.5.11 // indirect
-	github.com/Azure/go-autorest/autorest/azure/cli v0.4.5 // indirect
-	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
-	github.com/Azure/go-autorest/logger v0.2.1 // indirect
-	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
-	github.com/aws/aws-sdk-go-v2 v1.14.0 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.14.0 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.9.0 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.11.0 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.5 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.3.0 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.6 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ecr v1.15.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ecrpublic v1.12.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.8.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.10.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.15.0 // indirect
-	github.com/aws/smithy-go v1.11.0 // indirect
-	github.com/awslabs/amazon-ecr-credential-helper/ecr-login v0.0.0-20220228164355-396b2034c795 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/blendle/zapdriver v1.3.1 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/chrismellard/docker-credential-acr-env v0.0.0-20220119192733-fe33c00cee21 // indirect
 	github.com/cloudevents/sdk-go/v2 v2.11.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dimchansky/utfbom v1.1.1 // indirect
-	github.com/docker/cli v20.10.12+incompatible // indirect
-	github.com/docker/distribution v2.8.0+incompatible // indirect
-	github.com/docker/docker v20.10.12+incompatible // indirect
-	github.com/docker/docker-credential-helpers v0.6.4 // indirect
 	github.com/emicklei/go-restful v2.16.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/go-kit/log v0.1.0 // indirect
@@ -65,12 +35,10 @@ require (
 	github.com/go-openapi/jsonreference v0.19.5 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang-jwt/jwt/v4 v4.3.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/go-containerregistry v0.8.1-0.20220216220642-00c59d91847c // indirect
-	github.com/google/go-containerregistry/pkg/authn/kubernetes v0.0.0-20220301182634-bfe2ffc6b6bd // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
@@ -78,13 +46,11 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -93,13 +59,11 @@ require (
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/prometheus/statsd_exporter v0.21.0 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/automaxprocs v1.4.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
-	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
@@ -121,5 +85,4 @@ require (
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
 	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
-	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- pkg/resolver/wrap/resolver.go
+++ pkg/resolver/wrap/resolver.go
@@ -2,14 +2,21 @@ package wrap
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"strings"
 	"time"
 
+	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	clientset "github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
+	pipelineclient "github.com/tektoncd/pipeline/pkg/client/injection/client"
 	"github.com/tektoncd/pipeline/pkg/resolution/common"
 	"github.com/tektoncd/pipeline/pkg/resolution/resolver/framework"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes"
 	"knative.dev/pkg/client/injection/kube/client"
+	"knative.dev/pkg/logging"
+	"sigs.k8s.io/yaml"
 )
 
 // LabelValueWrapResolverType is the value to use for the
@@ -20,14 +27,44 @@ const LabelValueWrapResolverType string = "wrap"
 // admins (e.g. via ConfigMap)
 const timeoutDuration = time.Minute
 
+const (
+	PipelineRefParam = "pipelineref"
+	WorkspacesParam  = "workspaces"
+	TargetParam      = "target"
+	WrapperParam     = "wrapper"
+
+	DefaultBaseImage = "ghcr.io/openshift-pipelines/tekton-wrap-pipeline/base:latest"
+)
+
+type ResolvedWrapperResource struct {
+	Content     []byte
+	PipelineRef string
+}
+
+var _ framework.ResolvedResource = &ResolvedWrapperResource{}
+
+// Data returns the bytes of the file resolved from git.
+func (r *ResolvedWrapperResource) Data() []byte {
+	return r.Content
+}
+
+// Annotations returns the metadata that accompanies the resource fetched from the cluster.
+func (r *ResolvedWrapperResource) Annotations() map[string]string {
+	return map[string]string{
+		"PipelineRef": r.PipelineRef,
+	}
+}
+
 // Resolver implements a framework.Resolver that can "wrap" a Pipeline for not using a PVC for workspaces
 type Resolver struct {
-	kubeClientSet kubernetes.Interface
+	kubeClientSet     kubernetes.Interface
+	pipelineClientSet clientset.Interface
 }
 
 // Initialize sets up any dependencies needed by the Resolver. None atm.
 func (r *Resolver) Initialize(ctx context.Context) error {
 	r.kubeClientSet = client.Get(ctx)
+	r.pipelineClientSet = pipelineclient.Get(ctx)
 	return nil
 }
 
@@ -50,12 +87,172 @@ func (r *Resolver) GetSelector(context.Context) map[string]string {
 
 // ValidateParams ensures parameters from a request are as expected.
 func (r *Resolver) ValidateParams(ctx context.Context, params map[string]string) error {
-	fmt.Println("params", params)
-	// FIXME: to implement
-	return nil
+	_, err := populateParamsWithDefaults(ctx, params)
+	return err
 }
 
 // Resolve uses the given params to resolve the requested file or resource.
-func (r *Resolver) Resolve(ctx context.Context, params map[string]string) (framework.ResolvedResource, error) {
-	return nil, errors.New("Not implemented")
+func (r *Resolver) Resolve(ctx context.Context, origParams map[string]string) (framework.ResolvedResource, error) {
+	logger := logging.FromContext(ctx)
+
+	baseimage := DefaultBaseImage
+	namespace := common.RequestNamespace(ctx)
+	params, err := populateParamsWithDefaults(ctx, origParams)
+	if err != nil {
+		logger.Infof("wrap resolver parameter(s) invalid: %v", err)
+		return nil, err
+	}
+
+	pipeline, err := r.pipelineClientSet.TektonV1beta1().Pipelines(namespace).Get(ctx, params[PipelineRefParam], metav1.GetOptions{})
+	if err != nil {
+		logger.Infof("failed to load pipeline %s from namespace %s: %v", params[PipelineRefParam], namespace, err)
+		return nil, err
+	}
+
+	workspaces := sets.NewString(strings.Split(params[WorkspacesParam], ",")...)
+
+	// Resolve tasks from Pipeline to embedded and mutate them
+	taskSpecs, err := r.resolveTaskSpecs(ctx, &pipeline.Spec)
+	if err != nil {
+		logger.Infof("failed to resolve task specs from pipeline %s in namespace %s: %v", params[PipelineRefParam], namespace, err)
+		return nil, err
+	}
+
+	newPipeline := pipeline.DeepCopy()
+	wtargetimages := map[string]string{}
+	for _, w := range workspaces.List() {
+		wtargetimages[w] = strings.ReplaceAll(params[TargetParam], "{{workspace}}", w)
+	}
+
+	for i, t := range newPipeline.Spec.Tasks {
+		taskWorkspaces := make([]string, len(t.Workspaces))
+		for j, w := range t.Workspaces {
+			taskWorkspaces[j] = w.Workspace
+		}
+		// Skip if not using the workspace
+		if !workspaces.HasAny(taskWorkspaces...) {
+			continue
+		}
+
+		s := taskSpecs[t.Name]
+		// Except the first task, add a step to extract workspace content
+		if i != 0 {
+			var script strings.Builder
+			fmt.Fprintf(&script, "#!/busybox/sh -e\n")
+			for _, pw := range t.Workspaces {
+				if workspaces.Has(pw.Workspace) {
+					baseimage = wtargetimages[pw.Workspace]
+					var w v1beta1.WorkspaceDeclaration
+					for _, d := range s.Workspaces {
+						if d.Name == pw.Name {
+							w = d
+						}
+					}
+					fmt.Fprintf(&script, `echo "Extract workspace content from %s in %s"
+crane export %s | tar -x -C %s
+`, baseimage, w.GetMountPath(), baseimage, w.GetMountPath())
+				}
+			}
+			s.Steps = append([]v1beta1.Step{{
+				Name:       "import-workspace",
+				Image:      "gcr.io/go-containerregistry/crane:debug",
+				WorkingDir: "/",
+				Script:     script.String(),
+			}}, s.Steps...)
+		}
+
+		var script strings.Builder
+		fmt.Fprintf(&script, "#!/busybox/sh -e\n")
+		for _, pw := range t.Workspaces {
+			if workspaces.Has(pw.Workspace) {
+				if i != 0 {
+					baseimage = wtargetimages[pw.Workspace]
+				}
+				var w v1beta1.WorkspaceDeclaration
+				for _, d := range s.Workspaces {
+					if d.Name == pw.Name {
+						w = d
+					}
+				}
+				fmt.Fprintf(&script, `echo "Export workspace content from %s to %s"
+(cd %s && tar -f - -c . | crane append -b %s -t %s -f -)
+`, w.GetMountPath(), wtargetimages[pw.Workspace], w.GetMountPath(), baseimage, wtargetimages[pw.Workspace])
+			}
+		}
+		s.Steps = append(s.Steps, v1beta1.Step{
+			Name:       "export-workspace",
+			Image:      "gcr.io/go-containerregistry/crane:debug",
+			WorkingDir: "/",
+			Script:     script.String(),
+		})
+		newPipeline.Spec.Tasks[i].TaskRef = nil
+		newPipeline.Spec.Tasks[i].TaskSpec.TaskSpec = *s
+	}
+
+	newPipeline.Kind = "Pipeline"
+	newPipeline.APIVersion = "tekton.dev/v1beta1"
+	data, err := yaml.Marshal(newPipeline)
+	if err != nil {
+		logger.Infof("failed to marshal pipeline %s from namespace %s: %v", params[PipelineRefParam], namespace, err)
+		return nil, err
+	}
+
+	return &ResolvedWrapperResource{
+		Content:     data,
+		PipelineRef: params[PipelineRefParam],
+	}, nil
+}
+
+func (r *Resolver) resolveTaskSpecs(ctx context.Context, pipelineSpec *v1beta1.PipelineSpec) (map[string]*v1beta1.TaskSpec, error) {
+	taskSpecs := map[string]*v1beta1.TaskSpec{}
+	for _, t := range pipelineSpec.Tasks {
+		var taskSpec *v1beta1.TaskSpec
+		if t.TaskRef == nil {
+			// Embedded TaskSpec, get it straight
+			taskSpec = &t.TaskSpec.TaskSpec
+		} else {
+			var err error
+			taskSpec, err = r.getTaskSpec(ctx, t.Name)
+			if err != nil {
+				return nil, fmt.Errorf("couldn't fetch taskspec for %s: %v", t.Name, err)
+			}
+		}
+		taskSpecs[t.Name] = taskSpec
+	}
+	return taskSpecs, nil
+}
+
+func (r *Resolver) getTaskSpec(ctx context.Context, name string) (*v1beta1.TaskSpec, error) {
+	namespace := common.RequestNamespace(ctx)
+	t, err := r.pipelineClientSet.TektonV1beta1().Tasks(namespace).Get(ctx, name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return &t.Spec, nil
+}
+
+func populateParamsWithDefaults(ctx context.Context, params map[string]string) (map[string]string, error) {
+	conf := framework.GetResolverConfigFromContext(ctx)
+
+	var missingParams []string
+
+	if _, ok := params[WrapperParam]; !ok {
+		if wrapperVal, ok := conf["default-wrapper"]; !ok {
+			missingParams = append(missingParams, WrapperParam)
+		} else {
+			params[WrapperParam] = wrapperVal
+		}
+	}
+
+	if _, ok := params[PipelineRefParam]; !ok {
+		missingParams = append(missingParams, PipelineRefParam)
+	}
+	if _, ok := params[TargetParam]; !ok {
+		missingParams = append(missingParams, TargetParam)
+	}
+	if _, ok := params[WorkspacesParam]; !ok {
+		missingParams = append(missingParams, WorkspacesParam)
+	}
+
+	return params, nil
 }
